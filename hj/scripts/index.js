951b2081acc6696206e059beafbbd049413453ee
bysujin <bysuejin@gmail.com>
2023-09-03 15:05:53 +0900

    initial commit

---
+++ hj/scripts/index.js
@@ -0,0 +1,13 @@
+class Contact extends React.Component {
+    render() {
+        return (
+            <div style="text-align: center; color: #097AAA; font-size: 12px; font-family: MaruBuriOTF; font-weight: 400; line-height: 19.56px; word-wrap: break-word">
+                계좌번호
+            </div>
+        );
+    }
+}
+
+
+const contact = ReactDOM.createRoot(document.getElementById('contact'));
+contact.render(<h1>Hello, world!</h1>);
