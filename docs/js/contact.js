cd9edf9af4fe791065c193036bec45d50824115a
Han Suejin <143918619+bysujin@users.noreply.github.com>
2023-09-05 23:50:08 +0900

    copy fixed

--- docs/js/contact.js
+++ docs/js/contact.js
@@ -1,10 +1,18 @@
-const groom = ['01027709172', '농협<br/>901066-51-146288<br/>문혁준']
-const groom_mom = ['01089612374', '농협은행<br/>901066-52-225071<br/>김형영']
-const bride = ['01062019612', '우리은행<br/>1002-645-967586<br/>한혜진']
-const bride_mom = ['01051539612', '토스뱅크<br/>100033832559<br/>강영미']
-const bride_dad = ['01063329612', '신한은행<br/>110-111-740468<br/>한철영']
+const groom = ['01027709172', '농협은행<br/>901066-51-146288<br/>문혁준', '농협 901066-51-146288']
+const groom_mom = ['01089612374', '농협은행<br/>901066-52-225071<br/>김형영', '농협 901066-52-225071']
+const bride = ['01062019612', '우리은행<br/>1002-645-967586<br/>한혜진', '우리 1002-645-967586']
+const bride_mom = ['01051539612', '토스뱅크<br/>100033832559<br/>강영미', '토스 100033832559']
+const bride_dad = ['01063329612', '신한은행<br/>110-111-740468<br/>한철영', '신한 110-111-740468']
 var number_idx = 0
 var account_idx = 1
+var text_idx = 2
+
+function modalClose() {
+    copy_btn = document.getElementById('copy_btn')
+    copy_btn.innerHTML = '계좌번호 복사하기';
+    copy_svg = document.getElementById('copy_svg');
+    copy_svg.style.backgroundImage = "url('docs/svg/account.svg')";
+}
 
 function phoneCall(who) {
     switch(who) {
@@ -44,7 +52,7 @@ function copyToClipboard(text) {
     try {
         var successful = document.execCommand('copy');
         var msg = successful ? 'Text copied to clipboard' : 'Unable to copy text';
-        alert(msg);
+        // alert(msg);
     } catch (err) {
         console.error('Unable to copy text: ', err);
     }
@@ -55,9 +63,6 @@ document.getElementById("copy_btn").addEventListener("click", function() {
     text = document.getElementById('popup-p').innerText
     copy_btn = document.getElementById('copy_btn');
     copy_svg = document.getElementById('copy_svg');
-
-    copyToClipboard(text);
-
     copy_btn.innerHTML = '계좌번호 복사 완료!';
     copy_svg.style.backgroundImage = "url('docs/svg/check.svg')";
     copy_svg.style.width = '18px';
@@ -69,30 +74,37 @@ function popup(who) {
     switch(who) {
         case 'groom':
             number = groom[account_idx];
+            text_cpy = groom[text_idx];
             break;
         case 'groom_mom':
             number = groom_mom[account_idx];
+            text_cpy = groom_mom[text_idx];
             break;
         case 'bride':
             number = bride[account_idx];
+            text_cpy = bride[text_idx];
             break;
         case 'bride_mom':
             number = bride_mom[account_idx];
+            text_cpy = bride_mom[text_idx];
             break;
         case 'bride_dad':
             number = bride_dad[account_idx];
+            text_cpy = bride_dad[text_idx];
             break;
         default:
             number = '112'
             break
     }
+    copyToClipboard(text_cpy);
+
     var copy_svn = document.getElementById('copy_svg')
+    var copy_btn = document.getElementById('copy_btn')
     document.getElementById('popup-p').innerHTML = number
     copy_svn.style.backgroundImage = "url('docs/svg/account.svg')";
     copy_svg.style.width = '12px';
     copy_svg.style.height = '12px';
-    document.getElementById('copy_btn').innerHTML = "계좌번호 복사하기";
-    
-    console.log('popup')
-    console.log(who)
+    copy_btn.innerHTML = "계좌번호 복사하기";
+    console.log(copy_svn)
+    console.log(copy_btn)
 }
